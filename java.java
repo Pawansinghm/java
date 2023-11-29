e094ebe905c6e3192f69ca6d2fe735e0b9cfccab
Pawansinghm <143997560+Pawansinghm@users.noreply.github.com>
2023-11-29 14:03:47 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
-public class MultiplicationOfTwoNumbers {
-   public static void main(String args[]){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the value of the first number ::");
-      int a = sc.nextInt();
-      System.out.println("Enter the value of the first number ::");
-      int b = sc.nextInt();
-      int result = a*b;
-      System.out.println("Product of the given two numbers is ::"+result);
-   }
-}
+This is one 
+   This is two 
+   this is three
+   this is four
+   this is five
+   this is six 
+   this is seven
+   this is eight
+   this is nine
+   this is ten
